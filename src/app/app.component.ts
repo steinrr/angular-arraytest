513643ee655afee30f6a8e48122e5fce9cc9e733
steinrr <steinrr@gmail.com>
2021-05-12 07:27:25 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = '___Angular ' + VERSION.major;
+  name = '***Angular ' + VERSION.major;
 
   monthLabels = [
     'Jan',
